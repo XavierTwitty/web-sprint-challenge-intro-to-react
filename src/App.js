55ec00466ce00493da75d95c9147d6fefdacfc76
XavierTwitty <x.twitty@gmail.com>
2021-01-10 19:06:15 -0500

    added structure and dependecies no data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import "./App.css";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  const [character, setCharacter] = useState();
+  const [error, setError] = useState();
+
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios
+      .get()
+      .then()
+      .catch((err) => setError(err.response.message));
+  }, []);
+
+  useEffect(() => {
+    console.log("my character data", character);
+  }, [character]);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
     </div>
   );
-}
+};
 
 export default App;
